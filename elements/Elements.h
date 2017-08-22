ea84f614272392ef91fd12bcff3732824bc28e81
akalari <aditya.kalari@gmail.com>
2017-08-21 19:58:25 -0700

    Update game resources, enums

---
+++ elements/Elements.h
@@ -0,0 +1,8 @@
+#ifndef RESOURCES_H
+#define RESOURCES_H
+
+  enum Resources {BRICK, LUMBER, WOOL, GRAIN, ORE};
+  enum Color {RED, BLUE, WHITE, ORANGE}
+  enum Development {KNIGHT, ROAD, YEAR_OF_PLENTY, MONOPOLY, VICTORY_POINT}
+
+#endif
