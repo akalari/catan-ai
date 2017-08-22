21aeb7ddd117ffe7243bf7cbc058143df2063524
akalari <aditya.kalari@gmail.com>
2017-08-21 19:58:36 -0700

    First iteration of player class

---
+++ player/Player.cpp
@@ -0,0 +1,52 @@
+#include <cstddef>
+#include <iostream>
+#include <csdtio>
+#include <vector>
+
+#include "Player.h"
+#include "../board/Board.h"
+
+using namespace std;
+
+Player::Player (int c) {
+  color = c;
+}
+
+void addResCard(int e) {
+  resHand.push_back(e);
+}
+
+void takeResCard(int e) {
+  auto loc = find(resHand.begin(), resHand.end(), e);
+  if (loc != resHand.end()) resHand.erase(loc);
+}
+
+void addDevCard(int e) {
+  devHand.push_back(e);
+}
+
+void takeDevCard(int e) {
+  auto loc = find(devHand.begin(), devHand.end(), e);
+  if (loc != devHand.end()) devHand.erase(loc);
+}
+
+void placeRoad(Edge e) {
+  e.road = color;
+  road--;
+}
+
+void placeSettlement(Corner c) {
+  c.settlement = color;
+  settlement--;
+  score++;
+}
+
+void placeCity(Corner c) {
+  if (c.settlement == color) {
+    c.city = color;
+  }
+  c.settlement = -1;
+  settlement++;
+  city--;
+  score++;
+}
---
+++ player/Player.h
@@ -0,0 +1,33 @@
+#include <cstddef>
+#include <iostream>
+#include <csdtio>
+#include <vector>
+
+#include "../board/Board.h"
+
+using namespace std;
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+class Player {
+  private:
+    vector<int> resHand;
+    vector<int> devHand;
+    int numRoads = 15;
+    int numSettlements = 5;
+    int numCities = 4;
+    int color = -1;
+    int score = 0;
+  public:
+    void addResCard();
+    void takeResCard();
+    void addDevCard();
+    void takeDevCard();
+    void placeRoad(Edge e);
+    void placeSettlement(Corner c);
+    void placeCity(Corner c);
+    Player () {}
+};
+
+#endif
