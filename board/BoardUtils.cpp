7d535d85cb107450a4e7c74cfc47542823412117
Akhil Palla <akhil@theakhil.com>
2017-08-25 00:48:12 -0700

    Add util functions for verification before settlement building

--- board/BoardUtils.cpp
+++ board/BoardUtils.cpp
@@ -5,6 +5,35 @@
 
 extern Tile tiles[NUM_TILES];
 
+Corner* getOtherCorner(Edge* e, Corner* c);
+bool isTwoAway(Corner* settlement);
+
+/**
+ * Checks if a settlement (or settlement spot)
+ * is >= 2 spots away from other settlements
+ */
+bool isTwoAway(Corner* settlement) {
+
+	// Check all adjacent edges
+	for (Edge *e : settlement->adjEdges) {
+		Corner* c = getOtherCorner(e, settlement);
+		// If there is a settlement, return false
+		if (c->settlement > -1) return false;
+	}
+	return true;
+}
+
+/**
+ * Returns the corner connected to the edge
+ * that is not *c
+ */
+Corner* getOtherCorner(Edge* e, Corner* c) {
+  
+	Corner* front = e->adjCorners.front();
+	if (front == c) return e->adjCorners.back();
+	else return front;
+}
+
 /**
  * Returns the list of occupied settlements adjacent
  * to a tile with a specified number
@@ -18,5 +47,17 @@ std::vector<Corner*> getSettlements(int number) {
 		}
 	}
 	return matches;
+}
+
+
+/**
+ * Checks if a new settlement is adjacent to a road
+ * occupied by the player
+ */
+boolean adjOwnRoad(Corner* settlement, int player) {
+	for (Edge* e : settlement->adjEdges) {
+		if(e->road != -1 && e-> road == player) return true;
+	}
+	return false;
 }
 
