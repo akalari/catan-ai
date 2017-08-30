e4a34aff7e3e13fa0a31423984e77a2b371d6551
Akhil Palla <akhil@theakhil.com>
2017-08-29 23:03:19 -0700

    Add option to randomly generate board

--- board/SetupBoard.cpp
+++ board/SetupBoard.cpp
@@ -1,15 +1,20 @@
-#include <iostream>
-#include <cstdio>
+#include <iostream> // std::cout
+#include <array> // std::array
+#include <algorithm> // std::shuffle
+#include <random> // std::default_random_engine
 
 #include "SetupBoard.h"
 #include "Board.h"
 #include "../elements/Elements.h"
 
 extern Tile tiles[NUM_TILES];
+extern Edge edges[NUM_EDGES];
+extern Corner corners[NUM_CORNERS];
 
 int main() {
     initBoard();
-    inputBoard(TILE_POSSIBILITIES);
+	randomBoard(TILE_POSSIBILITIES);
+	printBoard();
     return 0;
 }
 
@@ -39,11 +44,32 @@ int getElement(char tc) {
     }
 }
 
+void randomBoard(const int possibilitiesSpiral[18]) {
+	std::array<char,19> tc {'b','b','b','o','o','o','l','l','l','l',
+		'w','w','w','w','g','g','g','g','d'};
+	int ti[NUM_TILES] {0};
+
+	auto rng = std::default_random_engine {};
+	std::shuffle(tc.begin(), tc.end(), rng);
+
+	int c =0;
+	for (int i = 0; i < NUM_TILES; i++) {
+      if(tc[TILES_SPIRAL[i]] == 'd')continue;
+      ti[TILES_SPIRAL[i]] = possibilitiesSpiral[c++];
+	}
+
+	std::cout << "Random Board: " << std::endl;
+	printTiles(tc.data(), ti);
+
+	loadBoard(tc.data(), ti);
+
+}
+
 /**
  * Allows the user to input the values for the board resources and probabilities
  */
 void inputBoard() {
-  char tc[NUM_TILES] = {0};
+ char tc[NUM_TILES] = {0};
   int ti[NUM_TILES] {0};
 
   for(int i = 0; i < NUM_TILES; i++) {
--- board/SetupBoard.h
+++ board/SetupBoard.h
@@ -4,6 +4,7 @@
 #include "Board.h"
 
 void printTiles(char tc[NUM_TILES], int ti[NUM_TILES]);
+void randomBoard(const int possibilitiesSpiral[18]);
 void inputBoard();
 void inputBoard(const int possibilitiesSpiral[18]);
 void loadBoard(char tc[NUM_TILES], int ti[NUM_TILES]);
