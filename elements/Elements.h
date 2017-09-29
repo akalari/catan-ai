6c10c2d664b037dc7d629531b2afe7ca7709acd1
Akhil Palla <akhil@theakhil.com>
2017-09-29 00:00:56 -0700

    Update constant array declaration

--- elements/Elements.h
+++ elements/Elements.h
@@ -1,9 +1,13 @@
 #ifndef RESOURCES_H
 #define RESOURCES_H
 
+#include <string>
+
+using namespace std;
+
 enum Resources {BRICK,LUMBER, WOOL, GRAIN, ORE};
-enum Color {RED, BLUE, WHITE, ORANGE};
-const string COLOR_STRINGS[4] {"RED", "BLUE", "WHITE", "ORANGE"};
+enum Color {RED, BLUE, WHITE, YELLOW};
+const string COLOR_STRINGS[4] = {"RED", "BLUE", "WHITE", "YELLOW"};
 enum Development {KNIGHT, ROAD, PLENTY, MONOPOLY, VP};
 
 #endif
